a4cda604bd323dfbc7b7125da3414ebcc5fb8882
Arpit Verma <arpitv970@gmail.com>
2024-11-12 22:42:51 +0530

    updated with basic as-integration/next

---
+++ app/api/graphql/route.ts
@@ -0,0 +1,13 @@
+import { startServerAndCreateNextHandler } from "@as-integrations/next";
+import { ApolloServer } from "@apollo/server";
+import { typeDefs } from "@/lib/graphql/schema";
+import { resolvers } from "@/lib/graphql/resolvers";
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const handler = startServerAndCreateNextHandler(server);
+
+export { handler as GET, handler as POST };
