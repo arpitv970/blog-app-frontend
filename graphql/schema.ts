ac553cadbd54146ec79cef739b4803dce77742f7
Arpit Verma <arpitv970@gmail.com>
2024-12-14 21:12:50 +0530

    Squashed commit of the following:
    commit 4ef8ef816f5f23b707b660dfdf257324bf9c10ad
    Author: Arpit Verma <arpitv970@gmail.com>
    Date:   Sat Dec 14 21:11:30 2024 +0530
    
        Major shift
    
        - Removed GraphQL & its dependencies
        - Updated the Docs

--- graphql/schema.ts
+++
@@ -1,8 +0,0 @@
-import "@/graphql/types/User";
-import "@/graphql/types/Blog";
-import "@/graphql/types/Bookmark";
-import "@/graphql/types/Like";
-import "@/graphql/types/Comment";
-import { builder } from "@/graphql/builder";
-
-export const schema = builder.toSchema();
